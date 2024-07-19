47acc0d47630ec3ec062725156558dc6de4a69c8
mohd123vaseem <muhammad123vaseem@gmail.com>
2024-07-19 09:46:19 +0530

    final commit

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,25 @@
+import   express  from 'express';
+import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
+import { categoryControlller, createCategoryController, deleteCategoryCOntroller, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
+
+
+const router = express.Router()
+
+//route
+
+//create-category
+router.post('/create-category',requireSignIn,isAdmin,createCategoryController)
+
+//update-category
+router.put("/update-category/:id",requireSignIn,isAdmin,updateCategoryController)
+
+//getAll category
+router.get("/get-category",categoryControlller);
+
+//single-category
+router.get("/single-category/:id",singleCategoryController);
+
+//delete-category
+router.delete("/delete-category/:id",requireSignIn,isAdmin,deleteCategoryCOntroller);
+
+export default router;
