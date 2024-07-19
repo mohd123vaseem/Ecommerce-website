51c2c17b35adfd7de1ffba81c65d0f862f09aa2e
mohd123vaseem <106667385+mohd123vaseem@users.noreply.github.com>
2024-07-19 10:22:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import colors from "colors";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    // console.log(
-    //   `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
-    // );
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
   } catch (error) {
     console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
